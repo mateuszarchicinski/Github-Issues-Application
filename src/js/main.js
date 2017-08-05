04898071ec8ee8c6530a2a2cdaca85f6e7bed3c6
mateuszarchicinski <mateusz.archicinski@gmail.com>
2017-08-05 21:18:02 +0200

    Added 'use strict' mod and some small improvements

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,35 @@
 (function () {
+    'use strict';
 
 
     // FLEXBOX & SCROLLBAR SUPPORT CHECK
     var flexStyle = document.createElement('div').style.flex,
         isWebkit = /WebKit/.test(navigator.userAgent),
-        bodyElem = document.getElementsByTagName('body')[0];
+        bodyElem = document.getElementsByTagName('body')[0],
+        headElem = document.getElementsByTagName('head')[0];
 
-    if (flexStyle === undefined) {
+    function createLinkElem(href) {
+        if (typeof href !== 'string') {
+            return;
+        }
+
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = href;
+
+        return link;
+    }
+
+    if (flexStyle === undefined) { // Troszkę dłuższy temat :)
         bodyElem.classList.add('flexbox-unsupported');
+
+        headElem.appendChild(createLinkElem('css/flexbox-unsupport.css'));
     }
 
     if (isWebkit) {
         bodyElem.classList.add('scrollbar-supported');
+
+        headElem.appendChild(createLinkElem('css/scrollbar-support.css'));
     }
 
 
@@ -35,6 +53,16 @@
             alert('You clicked navigation button which should triger function to show ' + this.getAttribute('data-status') + ' Github issues');
         });
     }
+
+
+    // ISSUE FAVOURITE    
+    document.addEventListener('keypress', function (evt) {
+        var favElem = document.querySelector('.issue-favourite:focus input');
+
+        if (evt.keyCode == 13 && favElem) {
+            favElem.checked = !favElem.checked;
+        }
+    });
 
 
 })();
